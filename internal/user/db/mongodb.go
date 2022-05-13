bdb39e7969485aa4131415aaadd63744a6812237
Maxym Popov <maxympopov260630@gmail.com>
2022-05-13 21:36:02 +0300

    Add methods Create and FindOne to Mongo DB

--- internal/user/db/mongodb.go
+++ internal/user/db/mongodb.go
@@ -2,6 +2,9 @@ package db
 
 import (
 	"context"
+	"fmt"
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/mongo-go-driver/mongo"
 	"rest-app/internal/user"
 	"rest-app/package/logging"
@@ -13,11 +16,36 @@ type db struct {
 }
 
 func (d *db) Create(ctx context.Context, user user.User) (string, error) {
-	d.collection.InsertOne()
+	d.logger.Debug("created user")
+	result, err := d.collection.InsertOne(ctx, user)
+
+	if err != nil {
+		return "", fmt.Errorf("Error to created user  due to error: %v", err)
+	}
+	d.logger.Debug("convert InsertedID to ObjectID")
+	oid, ok := result.InsertedID.(primitive.ObjectID)
+	if ok {
+		return oid.Hex(), nil
+	}
+	d.logger.Trace(user)
+	return "", fmt.Errorf("error to convert object id to Hex: %s", oid)
 }
 
-func (d *db) FindOne(ctx context.Context, id string) (user.User, error) {
-	panic("implement me")
+func (d *db) FindOne(ctx context.Context, id string) (u user.User, err error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return u, fmt.Errorf("failed to converted hex to objectID. Hex: %s", id)
+	}
+	filter := bson.M{"_id": oid}
+	result := d.collection.FindOne(ctx, filter)
+	if result.Err() != nil {
+		//TODO 404
+		return u, fmt.Errorf("failed to find one user by id: %s due to error: %v", id, err)
+	}
+	if err = result.Decode(&u); err != nil {
+		return u, fmt.Errorf("failed to decode user (id: %s from DB due to error: %v", id, err)
+	}
+	return u, nil
 }
 
 func (d *db) Update(ctx context.Context, user user.User) error {
